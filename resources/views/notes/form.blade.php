7503705cb724f70aaeaecf9658b4640a98bababf
shogo-okugami <okugami365@gamil.com>
2020-12-19 23:03:53 +0900

    画面作成

--- resources/views/notes/form.blade.php
+++ resources/views/notes/form.blade.php
@@ -8,5 +8,6 @@
   <div class="c-form__item">
     <textarea class="c-form__textarea" rows="20" placeholder="Please write a dialry"></textarea>
   </div>
+  <button type="submit" class="c-btn--primary">submit</button>
 </form>
 @endsection
