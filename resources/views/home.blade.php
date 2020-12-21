af64c2d108a90a400584cf367e206a0c74faa664
shogo-okugami <okugami365@gamil.com>
2020-12-21 09:24:51 +0900

    ホーム画面修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,58 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="c-card">
-  <div class="c-card__body">
-    <div class="c-card__date">
-      <div class="c-card__date__inner">
-        12 / 14<span>2020</span>
-      </div>
-    </div>
-    <div class="c-card__text">
-      <p>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-      </p>
-    </div>
-  </div>
-</div>
 
+@foreach ( $notes as $note)
 <div class="c-card">
   <div class="c-card__body">
     <div class="c-card__date">
       <div class="c-card__date__inner">
-        12 / 14<span>2020</span>
+        {{ $note-><span>2020</span>
       </div>
     </div>
     <div class="c-card__text">
       <p>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
+        {{ $note->text }}
       </p>
     </div>
   </div>
 </div>
+@endforeach
 
-<div class="c-card">
-  <div class="c-card__body">
-    <div class="c-card__date">
-      <div class="c-card__date__inner">
-        12 / 14<span>2020</span>
-      </div>
-    </div>
-    <div class="c-card__text">
-      <p>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-        今日はとても寒い日でした。しかも、風が強くていつもより寒かった。<br>
-      </p>
-    </div>
-  </div>
-</div>
-</div>
 @endsection
