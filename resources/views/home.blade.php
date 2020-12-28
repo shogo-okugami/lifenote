0b94205db5954c3a6661678a40f2e99bdf016d58
shogo-okugami <okugami365@gamil.com>
2020-12-28 14:12:21 +0900

    ホーム画面修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,7 @@
 
 @section('content')
 
-@foreach ( $notes as $note)
-<div class="c-card">
-  <div class="c-card__body">
-    <div class="c-card__date">
-      <div class="c-card__date__inner">
-        {{ $note->month }} / {{ $note->date }}<span>{{ $note->day }}</span>
-      </div>
-    </div>
-    <div class="c-card__text">
-      <p>
-        {{ $note->text }}
-      </p>
-    </div>
-  </div>
+<div id="note" userId="@json(Auth::id())">
 </div>
-@endforeach
 
 @endsection
