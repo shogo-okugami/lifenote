2cbeaee9658f78415d214c2e09bfed6faf887535
shogo-okugami <okugami365@gamil.com>
2020-12-28 14:10:12 +0900

    カレンダーコンポーネント修正

--- resources/js/components/Calendar.jsx
+++ resources/js/components/Calendar.jsx
@@ -5,87 +5,88 @@ import months from '../months'
 
 const Calendar = () => {
 
-  const [date, setDate] = useState(new Date())
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1;
+    const [date, setDate] = useState(new Date())
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1;
 
-  const startDate = new Date(year, month - 1, 1) //月の最初の年月日を取得
+    const startDate = new Date(year, month - 1, 1) //月の最初の年月日を取得
 
-  const endDate = new Date(year, month, 0) //月の最後の年月日を取得 
-  const endDayCount = endDate.getDate() //月の末日を取得
-  const lastMonthEndDate = new Date(year, month - 1, 0)
-  const lastMonthEndDayCount = lastMonthEndDate.getDate()
-  const startDay = startDate.getDay()
+    const endDate = new Date(year, month, 0) //月の最後の年月日を取得
+    const endDayCount = endDate.getDate() //月の末日を取得
+    const lastMonthEndDate = new Date(year, month - 1, 0)
+    const lastMonthEndDayCount = lastMonthEndDate.getDate()
+    const startDay = startDate.getDay()
 
-  let dayCount = 1
+    let dayCount = 1
 
-  const changeNextMonth = () => {
-    month === 12 ? setDate(new Date(year + 1 , month  - 12)) : setDate(new Date(year , month ));
-  }
+    const changeNextMonth = () => {
+        month === 12 ? setDate(new Date(year + 1, month - 12)) : setDate(new Date(year, month));
+    }
 
-  const changePrevMonth = () => {
-    month === 1 ? setDate(new Date(year - 1 , month + 10)) : setDate(new Date(year , month - 2 ));
-  }
+    const changePrevMonth = () => {
+        month === 1 ? setDate(new Date(year - 1, month + 10)) : setDate(new Date(year, month - 2));
+    }
 
-  const calendarBody = () => {
+    const calendarBody = () => {
+
+        return (
+            <>
+                <tr>
+                    {
+                        weeks.map((day, index) => <td className="p-calendar__date" key={index}>{day}</td>)
+                    }
+                </tr>
+                {(() => {
+                    const rows = []
+                    for (let w = 0; w < 6; w++) {
+                        rows.push(<tr key={w}>
+                            {
+                                (() => {
+
+                                    const date = [];
+
+                                    for (let d = 0; d < 7; d++) {
+                                        if (w === 0 && d < startDay) {
+                                            let num = lastMonthEndDayCount - startDay + d + 1
+                                            date.push(<td key={d} className="p-calendar__date--disabled">{num}</td>)
+                                        } else if (dayCount > endDayCount) {
+                                            let num = dayCount - endDayCount
+                                            date.push(<td key={d} className="p-calendar__date--disabled">{num}</td>)
+                                            dayCount++
+                                        } else {
+                                            date.push(<td key={d} className="p-calendar__date">{dayCount}</td>)
+                                            dayCount++
+                                        }
+                                    }
+                                    return date
+                                })()
+                            }
+                        </tr>)
+                    }
+                    return rows
+                })()}
+            </>
+        )
+    }
 
     return (
-      <>
-        <tr>
-          {
-            weeks.map((day, index) => <td className="p-calendar__date" key={index}>{day}</td>)
-          }
-        </tr>
-        {(() => {
-          const rows = []
-          for (let w = 0; w < 6; w++) {
-            rows.push(<tr>
-              {
-                (() => {
-
-                  const date = [];
-
-                  for (let d = 0; d < 7; d++) {
-                    if (w === 0 && d < startDay) {
-                      let num = lastMonthEndDayCount - startDay + d + 1
-                      date.push(<td className="p-calendar__date--disabled">{num}</td>)
-                    } else if (dayCount > endDayCount) {
-                      let num = dayCount - endDayCount
-                      date.push(<td className="p-calendar__date--disabled">{num}</td>)
-                      dayCount++
-                    } else {
-                      date.push(<td className="p-calendar__date">{dayCount}</td>)
-                      dayCount++
-                    }
-                  }
-                  return date
-                })()
-              }
-            </tr>)
-          }
-          return rows
-        })()}
-      </>
-    )
-  }
-
-  return (
-    <div className="p-calendar">
-      <div className="p-calendar__heading">
-        <div className="p-calendar__switch--prev"><img onClick={() => changePrevMonth()} src="images/nav-left.svg" /></div>
-        <div>{months[month - 1]}</div>
-        <div>{year}</div>
-        <div className="p-calendar__switch--next"><img onClick={() => changeNextMonth()} src="images/nav-right.svg" /></div>
-      </div>
-      <table className="p-calendar__body">
-        <tbody>
-          {calendarBody()}
-        </tbody>
-      </table>
-    </div>
-  );
+        <div className="p-calendar">
+            <div className="p-calendar__heading">
+                <div className="p-calendar__switch--prev"><img onClick={() => changePrevMonth()} src="images/nav-left.svg" /></div>
+                <div>{months[month - 1]}</div>
+                <div>{year}</div>
+                <div className="p-calendar__switch--next"><img onClick={() => changeNextMonth()} src="images/nav-right.svg" /></div>
+            </div>
+            <table className="p-calendar__body">
+                <tbody>
+                    {calendarBody()}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 if (document.getElementById('calendar')) {
-  ReactDOM.render(<Calendar />, document.getElementById('calendar'));
+    ReactDOM.render(<Calendar />, document.getElementById('calendar'));
 }
+
