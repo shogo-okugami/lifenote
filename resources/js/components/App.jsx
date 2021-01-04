3fe59db045a8f2fb0142a5a95cf2feff1edc4289
shogo-okugami <okugami365@gmail.com>
2021-01-04 13:49:01 +0900

    修正 propsを設定 contextオブジェクト作成

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,55 +1,95 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, createContext, useContext } from 'react'
 import ReactDOM from 'react-dom'
 import Nav from './Nav'
 import NoteList from './NoteList'
 import Calendar from './Calendar'
 
+const settings = {
+  user_id: 1,
+  color_mode: 2,
+  font_style: 0,
+  dark_flg: 1,
+}
+
+const colors = ['normal', 'red', 'blue', 'yellow', 'green', 'pink']
+
+const colorMode = colors[settings.color_mode]
+
+const settingContext = createContext()
+
+const setColorMode = (color) => {
+
+  switch (color) {
+    case 'red':
+      return 'is-red'
+      break;
+    default:
+      return ''
+      break;
+  }
+
+}
+
 const App = (props) => {
 
-    const content = () => {
+  const [color, setColor] = useState(colorMode)
 
-        switch (props.content) {
+  const [isDark, setIsDark] = useState(Boolean(settings.dark_flg))
 
-            case 'note':
-                return <NoteList userId={1} />
-                break;
-            case 'calendar':
-                return <Calendar />
-                break
-            case 'diary':
-                return <Diary />
-                break
-            default:
-                return <NoteList userId={1} />
-                break
-        }
+  console.log('color is ' + color + ' dark is ' + isDark)
 
+  const content = () => {
+
+    switch (props.content) {
+
+      case 'note':
+        return <NoteList userId={props.userId} />
+        break;
+      case 'calendar':
+        return <Calendar />
+        break
+      case 'diary':
+        return <Diary />
+        break
+      default:
+        return <NoteList userId={props.userId} />
+        break
     }
 
-    return (
-        <>
-            <header id="header" className="l-header">
-                <div className="l-header__inner">
-                    <h1 className="c-heading--large">lifenote</h1>
-                    <div>
-                        login/register
-        </div>
-                </div>
-            </header>
+  }
 
-            <main id="main" className="l-wrapper">
-                <Nav />
-                <div className="l-wrapper__inner">
-                    {content()}
-                </div>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <settingContext.Provider value={{ colorMode: { color: color, setColor: setColor }, darkMode: { dark: isDark, setIsDark: setIsDark } }}>
+        <header id="header" className={'l-header ' + (isDark ? 'is-dark ' : '') + setColorMode(color)}>
+          <div className="l-header__inner">
+            <h1 className="c-heading--large">lifenote</h1>
+            <div>
+              { props.isLogin && <div>login</div>}
+            </div>
+          </div>
+        </header>
+
+        <main id="main" className="l-wrapper">
+          <Nav />
+          <div className="l-wrapper__inner">
+            {content()}
+          </div>
+        </main>
+      </settingContext.Provider>
+    </>
+  )
 }
 
 const Element = document.getElementById('app');
 if (Element) {
-    const content = Element.getAttribute('content');
-    const parsedContent = JSON.parse(content);
-    ReactDOM.render(<App content={parsedContent} />, Element);
+  const userId = Element.getAttribute('userId')
+  const parsedUserId = JSON.parse(userId)
+  const isLogin = Element.getAttribute('isLogin')
+  const parsedIsLogin = JSON.parse(isLogin)
+  const content = Element.getAttribute('content')
+  const parsedContent = JSON.parse(content)
+  ReactDOM.render(<App userId={parsedUserId} isLogin={parsedIsLogin} content={parsedContent} />, Element);
 }
+
+export { settingContext }
