45b61b2fd478f2743da48b02d8cd6e24f171e432
shogo-okugami <okugami365@gmail.com>
2021-01-09 23:56:01 +0900

    Blade修正 フォームをコンポーネント化

--- resources/views/notes/form.blade.php
+++ resources/views/notes/form.blade.php
@@ -1,17 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<form class="c-form" method="post" action="{{ route('notes.store') }}">
-  @csrf
-  <div class="c-form__item">
-    <input id="js-input-date" name="created_at" class="c-form__input js-flatpickr" value="{{ date('Y-m-d') }}">
-  </div>
-  <div class="c-form__item">
-    <textarea name="text" class="c-form__textarea" rows="18" placeholder="Please write a dialry"></textarea>
-    @error('text')
-      <div class="c-form__message">{{ $message }}</div>
-    @enderror
-  </div>
-  <button type="submit" class="c-btn--primary">submit</button>
-</form>
+<div id="app"
+ userId='@json(Auth::id())'
+ isLogin='@json(Auth::check())'
+ csrf='@json(csrf_token())'
+ content='@json('dialry')'
+ errors='@json($errors->toArray())'
+ date='@json(date('Y-m-d'))'
+ >
+</div>
 @endsection
