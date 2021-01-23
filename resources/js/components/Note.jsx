f50d8e88a84fd6734ed1ceb1305755b8933c535e
shogo-okugami <okugami365@gmail.com>
2021-01-23 23:14:25 +0900

    コンポーネント修正 Noteコンポーネントimport

--- resources/js/components/Note.jsx
+++ resources/js/components/Note.jsx
@@ -1,62 +1,9 @@
+import React from 'react'
 
-import React, { useContext } from 'react';
-import weeks from '../weeks'
-import { settingContext } from './App'
+const Note = ({note}) => {
 
-const Note = (props) => {
+    return <div>{note.created_at}{note.text}</div>
 
-    const note = props.note
-
-    const getNoteDate = (date) => {
-        let str = date.substr(0, 10)
-        str = str.split('-')
-        date = str.map(value => parseInt(value))
-        date = new Date(date[0], date[1] - 1, date[2])
-        const result = {}
-        result.day = date.getDate()
-        result.month = date.getMonth() + 1
-        result.year = date.getFullYear()
-        result.ofWeek = weeks[date.getDay()]
-        return result
-    }
-
-    const date = getNoteDate(note.created_at)
-
-    const useSettingContext = useContext(settingContext)
-
-    const isDark = useSettingContext.darkMode.isDark
-
-    const nl2br = (text) => {
-        let regex = /(\n)/g
-        return text.split(regex).map((line, index) => {
-            if (line.match(regex)) {
-                return <br key={index} />
-            }
-            else {
-                return line
-            }
-        }
-        )
-    }
-
-    return (
-        <>
-            <div key={note.id} className={'c-card ' + (isDark ? 'is-dark' : '')}>
-                <div className="c-card__body">
-                    <div className={'c-card__date ' + (isDark ? 'is-dark' : '')}>
-                        <div className="c-card__date__inner">
-                            {date.month}/{date.day} {date.ofWeek}<span className={isDark ? 'is-dark' : ''}>{date.year}</span>
-                        </div>
-                    </div>
-                    <div className="c-card__text">
-                        <div>
-                          {nl2br(note.text)}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
 }
 
 export default Note
