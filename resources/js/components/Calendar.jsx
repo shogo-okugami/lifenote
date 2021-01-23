be428da3b00a0f553022b299ca3d403ed11618f2
shogo-okugami <okugami365@gmail.com>
2021-01-23 23:09:31 +0900

    メソッド追加 switchContent フォームまたはノートを表示するメソッド

--- resources/js/components/Calendar.jsx
+++ resources/js/components/Calendar.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState, useRef, useContext } from 'react';
 import weeks from '../weeks'
 import months from '../months'
+import DialryForm from './DialryForm'
 import { settingContext } from './App'
 
-const Calendar = ({ userId, notes: index }) => {
+const Calendar = ({ userId, notes: index, errors, csrf, date: defaultDate }) => {
 
     const [date, setDate] = useState(new Date())
     const year = date.getFullYear()
     const month = date.getMonth() + 1;
     const [isOpen, setIsOpen] = useState(false)
     const menuRef = useRef(null)
+    const today = (() => {
 
+        let dt = new Date();
+        let y = dt.getFullYear();
+        let m = ("00" + (dt.getMonth() + 1)).slice(-2);
+        let d = ("00" + dt.getDate()).slice(-2);
+        let result = y + '-' + m + '-' + d;
+        return result;
+
+    })()
+
+    const pageFlag = document.documentElement.clientWidth > 800 ? true : false
+    console.log(pageFlag)
     const useSettingContext = useContext(settingContext)
     const isDark = useSettingContext.darkMode.isDark
 
@@ -34,19 +47,29 @@ const Calendar = ({ userId, notes: index }) => {
 
         (async () => {
 
-            try{
+            try {
 
-            const res = await fetch(`api/users/${userId}/notes/${String(year) + '-' + (month < 10 ? '0' + String(month) : String(month))}`)
-            const resp = await res.json()
+                const res = await fetch(`api/users/${userId}/notes/${String(year) + '-' + (month < 10 ? '0' + String(month) : String(month))}`)
+                const resp = await res.json()
 
-            setNotes(resp.data)
-            }catch(error){
+                setNotes(resp.data)
+            } catch (error) {
                 console.log(error)
             }
 
         })()
 
     }, [date])
+
+    useEffect(() => {
+
+        const elements = document.getElementsByClassName('js-target')
+
+        for (const element of elements) {
+            isDark ? element.classList.add('is-dark') : element.classList.remove('is-dark')
+        }
+
+    }, [isDark])
 
 
     const changeNextMonth = () => {
@@ -84,6 +107,34 @@ const Calendar = ({ userId, notes: index }) => {
 
     }
 
+    const [content, setContent] = useState(pageFlag)
+
+    const switchContent = async (paramId, ...array) => {
+
+        const paramDate = (() => {
+
+            const newArray = array.map(num => {
+                return num < 10 ? '0' + String(num) : String(num)
+            })
+
+            return newArray.join('-')
+
+        })()
+
+        try {
+            const res = await fetch(`api/users/${paramId}/note/${paramDate}`)
+            const resp = await res.json()
+            console.log(resp.data)
+            if (resp.data.length) {
+                setContent(true)
+            } else {
+                setContent(false)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     const calendarBody = (() => {
 
         const notesDates = notes.map(data => data.created_at.substr(0, 10))
@@ -98,7 +149,7 @@ const Calendar = ({ userId, notes: index }) => {
                 {(() => {
                     const rows = []
                     for (let w = 0; w < 6; w++) {
-                        rows.push(<tr key={w}>
+                        rows.push(<tr key={w} className={'p-calendar__row js-target'}>
                             {
                                 (() => {
 
@@ -110,16 +161,16 @@ const Calendar = ({ userId, notes: index }) => {
 
                                             let num = lastMonthEndDayCount - startDay + d + 1
                                             date.push(<td key={d}
-                                                onClick={() => redirect(userId, (month === 1 ? year - 1 : year), (month === 1 ? 12 : month - 1), num)}
-                                                className="p-calendar__date--disabled">{num}</td>)
+                                                onClick={() => pageFlag ? switchContent(userId, (month === 1 ? year - 1 : year), (month === 1 ? 12 : month - 1), num) : redirect(userId, (month === 1 ? year - 1 : year), (month === 1 ? 12 : month - 1), num)}
+                                                className={"p-calendar__date--disabled js-target"}>{num}</td>)
 
                                         } else if (dayCount > endDayCount) {
 
                                             let num = dayCount - endDayCount
 
                                             date.push(<td key={d}
-                                                onClick={() => redirect(userId, (month === 12 ? year + 1 : year), (month === 12 ? 1 : month + 1), num)}
-                                                className="p-calendar__date--disabled">{num}</td>)
+                                                onClick={() => pageFlag ? switchContent(userId, (month === 12 ? year + 1 : year), (month === 12 ? 1 : month + 1), num) : redirect(userId, (month === 12 ? year + 1 : year), (month === 12 ? 1 : month + 1), num)}
+                                                className={"p-calendar__date--disabled js-target"}>{num}</td>)
                                             dayCount++
 
                                         } else {
@@ -136,8 +187,8 @@ const Calendar = ({ userId, notes: index }) => {
                                             const flag = notesDates.includes(noteDate)
 
                                             date.push(<td key={d}
-                                                onClick={() => redirect(userId, year, month, day)}
-                                                className={"p-calendar__date" + (flag ? ' is-posted' : '')}
+                                                onClick={() => pageFlag ? switchContent(userId, year, month, day) : redirect(userId, year, month, day)}
+                                                className={"p-calendar__date" + (flag ? ' is-posted' : '') + (noteDate === today ? ' is-today' : '')}
                                             >{dayCount}
                                             </td>)
                                             dayCount++
@@ -155,27 +206,30 @@ const Calendar = ({ userId, notes: index }) => {
     })()
 
     return (
-        <div className={'p-calendar ' + (isDark ? 'is-dark' : '')}>
-            <div className="p-calendar__heading">
-                <div className="p-calendar__switch--prev"><img onClick={() => changePrevMonth()} src="images/nav-left.svg" /></div>
-                <div onClick={() => setIsOpen(!isOpen)}
-                    onBlur={() => setIsOpen(false)} ref={menuRef}
-                    tabIndex={0}
-                    className={'p-calendar__month ' + (isDark ? 'is-dark' : '')}>
-                    {months[month - 1]}
-                    <ul className={"p-calendar__month__menu"}
-                        style={isOpen ? { display: 'block' } : { display: 'none' }}>
-                        {months.map((month, index) => <li onClick={() => setDate(new Date(year, index))} key={index}>{month}</li>)}
-                    </ul>
+        <div className={'p-calendar u-flex js-target'}>
+            <div className='p-calendar__body'>
+                <div className="p-calendar__heading">
+                    <div className="p-calendar__switch--prev"><img onClick={() => changePrevMonth()} src="images/nav-left.svg" /></div>
+                    <div onClick={() => setIsOpen(!isOpen)}
+                        onBlur={() => setIsOpen(false)} ref={menuRef}
+                        tabIndex={0}
+                        className={'p-calendar__month js-target'}>
+                        {months[month - 1]}
+                        <ul className={"p-calendar__month__menu"}
+                            style={isOpen ? { display: 'block' } : { display: 'none' }}>
+                            {months.map((month, index) => <li onClick={() => setDate(new Date(year, index))} key={index}>{month}</li>)}
+                        </ul>
+                    </div>
+                    <div className="p-calendar__year">{year}</div>
+                    <div className="p-calendar__switch--next"><img onClick={() => changeNextMonth()} src="images/nav-right.svg" /></div>
                 </div>
-                <div className="p-calendar__year">{year}</div>
-                <div className="p-calendar__switch--next"><img onClick={() => changeNextMonth()} src="images/nav-right.svg" /></div>
+                <table>
+                    <tbody>
+                        {calendarBody}
+                    </tbody>
+                </table>
             </div>
-            <table className="p-calendar__body">
-                <tbody>
-                    {calendarBody}
-                </tbody>
-            </table>
+            {content ? <div>sample</div> : <DialryForm flag={true} errors={errors} userId={userId} csrf={csrf} isDark={isDark} date={defaultDate} />}
         </div>
     );
 }
