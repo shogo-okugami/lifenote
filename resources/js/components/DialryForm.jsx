4e1ea56b8f34ac252452a29038ac7aa1f6f73975
shogo-okugami <okugami365@gmail.com>
2021-01-24 23:43:08 +0900

    コンポーネント修正 js-flatpickr削除

--- resources/js/components/DialryForm.jsx
+++ resources/js/components/DialryForm.jsx
@@ -1,16 +1,14 @@
 import React, { useRef } from 'react'
 import TextArea from './TextArea'
 
-const DialryForm = ({userId,csrf,date,isDark,errors}) => {
+const DialryForm = ({ userId, csrf, date, isDark, errors ,flag}) => {
 
     return (
         <>
-            <form className={"c-form " + (isDark ? 'is-dark' : '')} method="post" action="http://localhost:8888/lifenote/public/notes">
+            <form className={"c-form " + (isDark ? 'is-dark' : '') + (flag ? ' is-arranged' : '')} method="post" action="http://localhost:8888/lifenote/public/notes">
                 <input type="hidden" name="_token" value={csrf} />
                 <input type="hidden" name="user_id" value={userId} />
-                <div className="c-form__item">
-                    <input id="js-input-date" name="created_at" className="c-form__input js-flatpickr" defaultValue={date} />
-                </div>
+                <input type="hidden" name="created_at" value={date} />
                 <TextArea errors={errors} />
                 <button type="submit" className="c-btn--primary">submit</button>
             </form>
