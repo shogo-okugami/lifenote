0369579db93b78ed3df04daf35d048f8d141af1f
shogo-okugami <okugami365@gmail.com>
2021-02-03 22:50:51 +0900

    blade修正 属性追加 notes カレンダーのノート csrf csrfトークン errors バリデーションエラー date 日付

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -5,7 +5,11 @@
 <div id="app"
 userId='@json(Auth::id())'
 isLogin='@json(Auth::check())'
-content='@json('calendar')'>
+content='@json('calendar')'
+notes='@json($notes)'
+csrf='@json(csrf_token())'
+errors='@json($errors->toArray())'
+date='@json(date('Y-m-d'))'>
 </div>
 
 @endsection
