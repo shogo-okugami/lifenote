9ce0281e0f193547e33bf855895a1616abf49741
shogo-okugami <okugami365@gmail.com>
2021-02-07 16:30:23 +0900

    Noteコンポーネント修正 更新ボタンと削除ボタンを追加

--- resources/js/components/Note.jsx
+++ resources/js/components/Note.jsx
@@ -1,10 +1,31 @@
-import { fromPairs } from 'lodash'
 import React from 'react'
 import { nl2br } from '../functions'
 
-const Note = ({ note }) => {
+const Note = ({ flag, note, csrf ,isDark }) => {
 
-    return <div>{nl2br(note.text)}</div>
+    return (
+        <>
+            <div className={'p-note' + (flag ? '--arranged' : '') + (isDark ? ' is-dark' : '')}>
+                <div className={'p-note__menu'}>
+                    <span className={'p-note__menu__icon'}>
+                        <img src={`http://localhost:8888/lifenote/public/images/edit${ isDark ? '--darked' : ''}.svg`} />
+                        <a href={`http://localhost:8888/lifenote/public/notes/${note.id}/edit`} />
+                    </span>
+                    <span className={'p-note__menu__icon'} >
+                        <img src={`http://localhost:8888/lifenote/public/images/delete${ isDark ? '--darked' : ''}.svg`} />
+                        <form method="post" action={`http://localhost:8888/lifenote/public/notes/${note.id}`}>
+                          <input type='hidden' name='_token' value={csrf} />
+                          <input type='hidden' name='_method' value='DELETE' />
+                          <button type='submit' />
+                        </form>
+                    </span>
+                </div>
+                <div className='p-note__text'>
+                    {nl2br(note.text)}
+                </div>
+            </div>
+        </>
+    )
 
 }
 
