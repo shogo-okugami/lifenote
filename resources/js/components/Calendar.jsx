fbeae8fd28b6043c1cbc2908b9f9b115dd849b7c
shogo-okugami <okugami365@gmail.com>
2021-02-10 20:06:39 +0900

    コンポーネント修正 useeffect削除

--- resources/js/components/Calendar.jsx
+++ resources/js/components/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import weeks from '../weeks'
 import Day from './Day'
 import MonthMenu from './MonthMenu'
