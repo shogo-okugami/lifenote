596766f6d2a980253c13c1113ce3b3a7332bc194
shogo-okugami <okugami365@gmail.com>
2021-02-10 20:01:30 +0900

    コンポーネント修正 Dayコンポーネントをimport

--- resources/js/components/Calendar.jsx
+++ resources/js/components/Calendar.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState, useRef, useContext, useMemo } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import weeks from '../weeks'
-import months from '../months'
+import Day from './Day'
 import MonthMenu from './MonthMenu'
+import YearSwitch from './YearSwitch'
 import DialryForm from './DialryForm'
 import Note from './Note'
 
-const Calendar = ({ userId, notes: index, errors, csrf, date: defaultDate, isDark }) => {
+const Calendar = ({ userId, mediaScreenL, notes: index, errors, csrf, date: defaultDate, isDark }) => {
 
     const [date, setDate] = useState(new Date())
     const year = date.getFullYear()
@@ -22,8 +23,6 @@ const Calendar = ({ userId, notes: index, errors, csrf, date: defaultDate, isDar
 
     })()
 
-    const pageFlag = document.documentElement.clientWidth > 800 ? true : false
-
     const [notes, setNotes] = useState(index)
     const notesDates = notes.map(note => note.created_at.substr(0, 10))
     const [note, setNote] = useState(notes.find(note => note.created_at.substr(0, 10) === today))
@@ -32,127 +31,75 @@ const Calendar = ({ userId, notes: index, errors, csrf, date: defaultDate, isDar
 
     const startDate = new Date(year, month - 1, 1) //月の最初の年月日を取得
 
+    const prevCount = startDate.getDay()
+
     const endDate = new Date(year, month, 0) //月の最後の年月日を取得
     const endDayCount = endDate.getDate() //月の末日を取得
     const lastMonthEndDate = new Date(year, month - 1, 0)
     const lastMonthEndDayCount = lastMonthEndDate.getDate()
     const startDay = startDate.getDay()
+    const nextCount = 6 - endDate.getDay()
+    const count = prevCount + nextCount + endDayCount
+    const length = Math.round(count / 7)
 
     let dayCount = 1
 
-    useEffect(() => {
+    const changeNextMonth = () => {
 
         (async () => {
 
+            const param = {
+                year: (month === 12 ? year + 1 : year),
+                month: (month === 12 ? month - 11 : month + 1),
+            }
+
+            const date = {
+                year: (month === 12 ? year + 1 : year),
+                month: (month === 12 ? month - 12 : month),
+            }
+
             try {
 
-                const res = await fetch(`api/users/${userId}/notes/${String(year) + '-' + (month < 10 ? '0' + String(month) : String(month))}`)
+                const res = await fetch(`api/users/${userId}/notes/${String(param.year) + '-' + (param.month < 10 ? '0' + String(param.month) : String(param.month))}`)
                 const resp = await res.json()
-
+                setDate(new Date(date.year, date.month))
                 setNotes(resp.data)
             } catch (error) {
                 console.log(error)
             }
 
         })()
-
-    }, [date])
-
-    const changeNextMonth = () => {
-        setDate(new Date((month === 12 ? year + 1 : year), (month === 12 ? month - 12 : month)))
     }
 
     const changePrevMonth = () => {
-        setDate(new Date((month === 1 ? year - 1 : year), (month === 1 ? month + 10 : month - 2)))
-    }
 
-    const redirect = async (paramId, ...array) => {
+        (async () => {
 
-        const paramDate = (() => {
-
-            const newArray = array.map(num => {
-                return num < 10 ? '0' + String(num) : String(num)
-            })
-
-            return newArray.join('-')
-
-        })()
-
-        try {
-            const res = await fetch(`api/users/${paramId}/note/${paramDate}`)
-            const resp = await res.json()
-            if (resp.data.length) {
-                window.location.href = 'http://localhost:8888/lifenote/public/home'
-            } else {
-                window.location.href = `http://localhost:8888/lifenote/public/notes/create/${paramDate}`
+            const param = {
+                year: (month === 1 ? year - 1 : year),
+                month: (month === 1 ? month + 11 : month - 1),
             }
-        } catch (error) {
-            console.log(error)
-        }
 
-    }
-
-    const switchContent = async (paramId, ...array) => {
-
-        const paramDate = (() => {
-
-            const newArray = array.map(num => {
-                return num < 10 ? '0' + String(num) : String(num)
-            })
-
-            return newArray.join('-')
-
-        })()
-
-        try {
-            const res = await fetch(`api/users/${paramId}/note/${paramDate}`)
-            const resp = await res.json()
-            if (resp.data.length) {
-                console.log(defaultDate)
-                setInputDateValue(today)
-                setNote(resp.data[0])
-                setContent(true)
-            } else {
-                setInputDateValue(paramDate)
-                setContent(false)
+            const date = {
+                year: (month === 1 ? year - 1 : year),
+                month: (month === 1 ? month + 10 : month - 2),
             }
-        } catch (error) {
-            console.log(error)
-        }
-    }
 
-    const findDate = (...array) => {
+            try {
 
-        const noteDate = (() => {
-            const newArray = array.map(num => {
-                return num < 10 ? '0' + String(num) : String(num)
-            })
+                const res = await fetch(`api/users/${userId}/notes/${String(param.year) + '-' + (param.month < 10 ? '0' + String(param.month) : String(param.month))}`)
+                const resp = await res.json()
+                setDate(new Date(date.year, date.month))
+                setNotes(resp.data)
+            } catch (error) {
+                console.log(error)
+            }
 
-            return newArray.join('-')
         })()
-        return notesDates.includes(noteDate)
-
     }
-
-    const getDate = (...array) => {
-
-        const noteDate = (() => {
-            const newArray = array.map(num => {
-                return num < 10 ? '0' + String(num) : String(num)
-            })
-
-            return newArray.join('-')
-        })()
-
-        return noteDate
-
-    }
-
-    console.log('render')
 
     const calendarBody = useMemo(
         () => {
-
             return (
                 <>
                     <tr>
@@ -162,7 +109,7 @@ const Calendar = ({ userId, notes: index, errors, csrf, date: defaultDate, isDar
                     </tr>
                     {(() => {
                         const rows = []
-                        for (let w = 0; w < 6; w++) {
+                        for (let w = 0; w < length; w++) {
                             rows.push(<tr key={w} className={'p-calendar__row' + (isDark ? ' is-dark' : '')}>
                                 {
                                     (() => {
@@ -173,34 +120,21 @@ const Calendar = ({ userId, notes: index, errors, csrf, date: defaultDate, isDar
 
                                             if (w === 0 && d < startDay) {
 
-                                                let num = lastMonthEndDayCount - startDay + d + 1
-                                                dates.push(<td key={d}
-                                                    onClick={() => pageFlag ? switchContent(userId, (month === 1 ? year - 1 : year), (month === 1 ? 12 : month - 1), num) : redirect(userId, (month === 1 ? year - 1 : year), (month === 1 ? 12 : month - 1), num)}
-                                                    className={'p-calendar__date--disabled' + (isDark ? ' is-dark' : '')}>{num}</td>)
+                                                const day = lastMonthEndDayCount - startDay + d + 1
+
+                                                dates.push(<Day userId={userId} year={(month === 1 ? year - 1 : year)} dayOfweek={d} month={(month === 1 ? 12 : month - 1)} day={day} disabled={true} isDark={isDark} mediaScreenL={mediaScreenL} today={today} notesDates={notesDates} key={d} setInputDateValue={setInputDateValue} setNote={setNote} setContent={setContent} />)
 
                                             } else if (dayCount > endDayCount) {
 
-                                                let num = dayCount - endDayCount
+                                                const day = dayCount - endDayCount
 
-                                                dates.push(<td key={d}
-                                                    onClick={() => pageFlag ? switchContent(userId, (month === 12 ? year + 1 : year), (month === 12 ? 1 : month + 1), num) : redirect(userId, (month === 12 ? year + 1 : year), (month === 12 ? 1 : month + 1), num)}
-                                                    className={'p-calendar__date--disabled' + (isDark ? ' is-dark' : '')}>{num}</td>)
+                                                dates.push(<Day userId={userId} year={(month === 12 ? year + 1 : year)} dayOfweek={d} month={(month === 12 ? 1 : month + 1)} day={day} disabled={true} isDark={isDark} mediaScreenL={mediaScreenL} today={today} notesDates={notesDates} key={d} setInputDateValue={setInputDateValue} setNote={setNote} setContent={setContent} />)
                                                 dayCount++
 
                                             } else {
                                                 const day = dayCount
 
-                                                const flag = findDate(year, month, day)
-
-                                                const date = getDate(year, month, day)
-
-                                                dates.push(<td key={d}
-                                                    onClick={() => pageFlag ? switchContent(userId, year, month, day) : redirect(userId, year, month, day)}
-                                                    className={'p-calendar__date' + (flag ? ' is-posted' : '')}
-                                                >
-                                                    <div className={(isDark ? 'is-dark' : '') + (date === today ? ' is-today' : '')}>{dayCount}</div>
-                                                    {flag && <span className={'js-target' + (isDark ? ' is-dark' : '')}></span>}
-                                                </td>)
+                                                dates.push(<Day userId={userId} year={year} dayOfweek={d} month={month} day={day} disabled={false} isDark={isDark} mediaScreenL={mediaScreenL} today={today} notesDates={notesDates} key={d} setInputDateValue={setInputDateValue} setNote={setNote} setContent={setContent} />)
                                                 dayCount++
                                             }
                                         }
@@ -213,16 +147,16 @@ const Calendar = ({ userId, notes: index, errors, csrf, date: defaultDate, isDar
                     })()}
                 </>
             )
-        }, [date,notes])
+        }, [date, notes, isDark])
 
     return (
         <div className={'p-calendar u-flex' + (isDark ? ' is-dark' : '')}>
             <div className='p-calendar__body'>
                 <div className="p-calendar__heading">
-                    <div className="p-calendar__switch--prev"><img className='icon' onClick={() => changePrevMonth()} src="images/nav-left.svg" /></div>
-                    <MonthMenu year={year} month={month} setDate={setDate} isDark={isDark} />
-                    <div className="p-calendar__year">{year}</div>
-                    <div className="p-calendar__switch--next"><img className='icon' onClick={() => changeNextMonth()} src="images/nav-right.svg" /></div>
+                    <div className="p-calendar__switch--prev"><img onClick={() => changePrevMonth()} src={`images/nav-left${isDark ? '--darked' : ''}.svg`} /></div>
+                    <MonthMenu userId={userId} year={year} month={month} setDate={setDate} setNotes={setNotes} isDark={isDark} />
+                    <YearSwitch userId={userId} year={year} month={month} setDate={setDate} setNotes={setNotes} isDark={isDark} />
+                    <div className="p-calendar__switch--next"><img onClick={() => changeNextMonth()} src={`images/nav-right${isDark ? '--darked' : ''}.svg`} /></div>
                 </div>
                 <table>
                     <tbody>
@@ -230,7 +164,7 @@ const Calendar = ({ userId, notes: index, errors, csrf, date: defaultDate, isDar
                     </tbody>
                 </table>
             </div>
-            {content ? <Note note={note} /> : <DialryForm flag={true} errors={errors} userId={userId} csrf={csrf} isDark={isDark} date={inputDateValue} />}
+            {content ? <Note flag={true} note={note} csrf={csrf} isDark={isDark} /> : <DialryForm flag={true} errors={errors} userId={userId} csrf={csrf} isDark={isDark} date={inputDateValue} />}
         </div>
     );
 }
