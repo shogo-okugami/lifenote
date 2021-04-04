beafe4e967630af770f51102cb94c3769db53fad
shogo-okugami <okugami365@gmail.com>
2021-04-04 09:41:05 +0900

    update:ノート更新処理に対応
    ノート更新の場合のPOST送信先
    name属性PUTのinput要素を追加

--- resources/js/components/DialryForm.jsx
+++ resources/js/components/DialryForm.jsx
@@ -7,8 +7,9 @@ const DialryForm = ({ userId, csrf, note = null, date, isDark, errors, flag }) =
 
     return (
         <>
-            <form className={"c-form " + (isDark ? 'is-dark' : '') + (flag ? ' is-arranged' : '')} method="post" action="http://localhost:8888/lifenote/public/notes">
+            <form className={"c-form " + (isDark ? 'is-dark' : '') + (flag ? ' is-arranged' : '')} method="post" action={`http://localhost:8888/lifenote/public/notes${editFlag ? `/${userId}` : ''}`}>
                 <input type="hidden" name="_token" value={csrf} />
+                {editFlag && <input type="hidden" name="_method" value="PUT" />}
                 <input type="hidden" name="user_id" value={userId} />
                 <input type="hidden" name="created_at" value={date} />
                 <TextArea errors={errors} text={editFlag ? note.text : null} />
