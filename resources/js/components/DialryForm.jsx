387924c00ba1be29a9003b42491f7a5c77b1d200
shogo-okugami <okugami365@gmail.com>
2021-04-07 13:37:10 +0900

    fix:ノート編集画面でformタグのaction属性が正常でない不具合を修正

--- resources/js/components/DialryForm.jsx
+++ resources/js/components/DialryForm.jsx
@@ -7,7 +7,7 @@ const DialryForm = ({ userId, csrf, note = null, date, isDark, errors, flag }) =
 
     return (
         <>
-            <form className={"c-form " + (isDark ? 'is-dark' : '') + (flag ? ' is-arranged' : '')} method="post" action={`http://localhost:8888/lifenote/public/notes${editFlag ? `/${userId}` : ''}`}>
+            <form className={"c-form " + (isDark ? 'is-dark' : '') + (flag ? ' is-arranged' : '')} method="post" action={'http://localhost:8888/lifenote/public/notes' + (editFlag ? `/${note.id}` : '')}>
                 <input type="hidden" name="_token" value={csrf} />
                 {editFlag && <input type="hidden" name="_method" value="PUT" />}
                 <input type="hidden" name="user_id" value={userId} />
