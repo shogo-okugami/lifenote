796ddf636cf869e37538eda278f20e03883d8b0b
shogo-okugami <okugami365@gmail.com>
2021-04-22 23:20:07 +0900

    update:mysqlのタイムゾーンを削除
    mysqlのタイムゾーンを指定しない

--- config/database.php
+++ config/database.php
@@ -61,7 +61,6 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-            'timezone' => '+09:00',
         ],
 
         'pgsql' => [
