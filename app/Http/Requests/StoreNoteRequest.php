055e175f2f548cd9971c832700edc123d7fa06fe
shogo-okugami <okugami365@gmail.com>
2021-04-26 20:36:05 +0900

    update:authorizeメソッドでリクエストの認可判定を修正
    認証済ユーザーのIDとリクエストのユーザーIDの値を比較する

--- app/Http/Requests/StoreNoteRequest.php
+++ app/Http/Requests/StoreNoteRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class StoreNoteRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class StoreNoteRequest extends FormRequest
    */
   public function authorize()
   {
-    return true;
+    return (int)$this->user_id === Auth::id();
   }
 
   /**
