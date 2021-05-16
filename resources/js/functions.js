d3bcc1cbf3b1d0744c75b1691024bab3d50b3d76
shogo-okugami <okugami365@gmail.com>
2021-05-16 13:27:11 +0900

    update:const routeを定義 引数nameをプロパティにroutesから対応するURLを出力

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -1,22 +1,47 @@
 import React from 'react'
+import { domain, routes } from './routes'
 
 //改行コード変換関数
 const nl2br = (text) => {
-  let regex = /(\n)/g
-  return text.split(regex).map((line, index) => {
-    if (line.match(regex)) {
-      return <br key={index} />
+    let regex = /(\n)/g
+    return text.split(regex).map((line, index) => {
+        if (line.match(regex)) {
+            return <br key={index} />
+        }
+        else {
+            return line
+        }
     }
-    else {
-      return line
-    }
-  }
-  )
+    )
 }
 
 //オブジェクト型判定関数
 const isObject = (value) => {
-  return value !== null && typeof value === 'object'
+    return value !== null && typeof value === 'object'
 }
 
-export { nl2br }
+//URL出力関数
+const route = (name, params) => {
+    //routesから値を取得
+    let value = routes[name]
+    //値がオブジェクトの場合にそのオブジェクトからパスを取得する
+    value = isObject(value) ? value.path : value
+    //paramsが配列で中に値を持つ場合
+    if (Array.isArray(params) && params.length) {
+        // paramsの値を各パラメータにセット
+        params.map((param) => {
+            value = value.replace(/\{.+\}/, String(param))
+        })
+    }
+    //paramsがないがパスにデフォルトパラメータがある場合
+    if (value.match(/\{.+\?\}/g)) {
+        //value.paramsのデフォルト値を各パラメータにセット
+        routes[name].params.map((param) => {
+            value = routes[name].path.replace(/\{.+\}/, String(param))
+        })
+    }
+    const result = domain + value
+    return result
+}
+
+export { nl2br, route }
