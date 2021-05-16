e5c257902e7f8bf5f643bc9b46ba955fad7df75d
shogo-okugami <okugami365@gmail.com>
2021-05-16 11:52:29 +0900

    update:const isObjectを定義 引数の値がオブジェクト型であるか判定する

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
+//改行コード変換関数
 const nl2br = (text) => {
-    let regex = /(\n)/g
-    return text.split(regex).map((line, index) => {
-        if (line.match(regex)) {
-            return <br key={index} />
-        }
-        else {
-            return line
-        }
+  let regex = /(\n)/g
+  return text.split(regex).map((line, index) => {
+    if (line.match(regex)) {
+      return <br key={index} />
     }
-    )
+    else {
+      return line
+    }
+  }
+  )
+}
+
+//オブジェクト型判定関数
+const isObject = (value) => {
+  return value !== null && typeof value === 'object'
 }
 
 export { nl2br }
