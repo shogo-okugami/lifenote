4dbe8f3f5902ee8c6c5ef28248c686cf2389cd87
shogo-okugami <okugami365@gmail.com>
2021-05-17 15:37:32 +0900

    update:href属性でroute関数を実行 各メニューのaタグのhref属性でroute関数を実行

--- resources/js/components/Nav.jsx
+++ resources/js/components/Nav.jsx
@@ -1,22 +1,17 @@
 import React, { useEffect } from 'react'
+import { route } from '../functions'
 
-const Nav = ({ csrf, isDark, mediaScreenL }) => {
+const Nav = ({ mediaScreenL, isDark }) => {
 
     useEffect(() => {
 
-        document.addEventListener('scroll', handeleScroll)
+        document.addEventListener('scroll', handleScroll)
 
-        return () => document.removeEventListener('scroll', handeleScroll)
+        return () => document.removeEventListener('scroll', handleScroll)
 
     }, [])
 
-    useEffect(() => {
-
-        document.body.style.background = isDark ? '#111' : '#fff'
-
-    }, [isDark])
-
-    const handeleScroll = () => {
+    const handleScroll = () => {
 
         const nav = document.getElementById('nav')
 
@@ -32,8 +27,7 @@ const Nav = ({ csrf, isDark, mediaScreenL }) => {
 
         const headerHeight = header.offsetHeight
 
-        if (window.pageYOffset > headerHeight && !nav.classList.contains('is-bottom')) {
-
+        if (window.pageYOffset > headerHeight && !nav.classList.contains('is-bottom') && mediaScreenL) {
             nav.classList.add('is-fixed')
             wrapper.style.marginLeft = navWidth + 'px'
             main.style.width = mainWidth + 'px'
@@ -47,12 +41,12 @@ const Nav = ({ csrf, isDark, mediaScreenL }) => {
 
     return (
         <>
-            <nav id="nav" className={'l-nav ' + (isDark ? 'is-dark' : '') + (!mediaScreenL ? ' is-bottom' : '')}>
+            <nav id="nav" className={'l-nav' + (isDark ? ' is-dark' : '') + (!mediaScreenL ? ' is-bottom' : '')}>
                 <ul className={"c-list" + (!mediaScreenL ? ' is-row' : '')}>
-                    <li className={'c-list__item ' + (isDark ? 'is-dark' : '') + (!mediaScreenL ? ' is-row' : '')}><a href="http://localhost:8888/lifenote/public/">Home</a></li>
-                    <li className={'c-list__item ' + (isDark ? 'is-dark' : '') + (!mediaScreenL ? ' is-row' : '')}><a href="http://localhost:8888/lifenote/public/calendar">Calendar</a></li>
-                    <li className={'c-list__item ' + (isDark ? 'is-dark' : '') + (!mediaScreenL ? ' is-row' : '')}><a href="http://localhost:8888/lifenote/public/notes/create">Dialry</a></li>
-                    <li className={'c-list__item ' + (isDark ? 'is-dark' : '') + (!mediaScreenL ? ' is-row' : '')}><form method="post" action="http://localhost:8888/lifenote/public/logout"><button type="submit" name="_token" value={csrf}>Logout</button></form></li>
+                    <li className={'c-list__item ' + (isDark ? ' is-dark' : '') + (!mediaScreenL ? ' is-row' : '')}><a href={route('home')}>Home</a></li>
+                    <li className={'c-list__item ' + (isDark ? ' is-dark' : '') + (!mediaScreenL ? ' is-row' : '')}><a href={route('calendar')}>Calendar</a></li>
+                    <li className={'c-list__item ' + (isDark ? ' is-dark' : '') + (!mediaScreenL ? ' is-row' : '')}><a href={route('notes.create')}>Dialry</a></li>
+                    <li className={'c-list__item ' + (isDark ? ' is-dark' : '') + (!mediaScreenL ? ' is-row' : '')}><a href={route('settings')}>Settings</a></li>
                 </ul>
             </nav>
         </>
