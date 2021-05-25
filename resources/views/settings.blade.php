67d5dab753349da880b2a8324155577dec82fe85
shogo-okugami <okugami365@gmail.com>
2021-05-25 20:39:27 +0900

    add:Settingsを描画するbladeファイル contentにsettingsを指定しSettingsを描画

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div id="app"
+ userId='@json(Auth::id())'
+ isLogin='@json(Auth::check())'
+ csrf='@json(csrf_token())'
+ content='@json('settings')'>
+</div>
+
+@endsection
