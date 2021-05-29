4b2afe6cfb76333f29e68e260e34366282e673a2
shogo-okugami <okugami365@gmail.com>
2021-05-29 21:58:08 +0900

    update:ホーム画面のルーティングを変更 NoteController@indexを実行

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home')->middleware('auth');
+Route::get('/', 'NoteController@index')->name('home')->middleware('auth');
 
 Auth::routes();
 
