0600367cd95f07b88d9e37e28568ba3d4f121d2d
shogo-okugami <okugami365@gmail.com>
2021-05-29 22:06:51 +0900

    update:LogoutButtonをレンダー LogoutButtonをimportしてレンダーする

--- resources/js/components/Settings/Settings.jsx
+++ resources/js/components/Settings/Settings.jsx
@@ -1,30 +1,27 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { AppSettings } from '../App'
-import { route } from '../../functions'
 import ThemeSetting from './ThemeSetting'
 import FontSetting from './FontSetting'
 import DarkSetting from './DarkSettting'
+import DeleteAccount from './DeleteAccount'
+import LogoutButton from './LogoutButton'
 
 const Settings = () => {
-    const csrf = useContext(AppSettings).csrf
     const isDark = useContext(AppSettings).darked.isDark
     const [isOver, setIsOver] = useState(false)
 
-return (
-    <>
-        <div className={'p-settings' + (isDark ? ' is-dark' : '')}>
-            <ThemeSetting setIsOver={setIsOver} />
-            <FontSetting setIsOver={setIsOver} />
-            <DarkSetting />
-            <form className='u-mb30' action={route('logout')} method='post'>
-                <input type='hidden' name='_token' value={csrf} />
-                <button className='c-btn--secondary'>Logout</button>
-            </form>
-            <button className='c-btn--danger'>Delete your account</button>
-        </div>
-        {isOver && <span className='c-overlay' />}
-    </>
-)
+    return (
+        <>
+            <div className={'p-settings' + (isDark ? ' is-dark' : '')}>
+                <ThemeSetting setIsOver={setIsOver} />
+                <FontSetting setIsOver={setIsOver} />
+                <DarkSetting />
+                <LogoutButton />
+                <DeleteAccount setIsOver={setIsOver} />
+            </div>
+            {isOver && <span className='c-overlay' />}
+        </>
+    )
 }
 
 export default Settings
