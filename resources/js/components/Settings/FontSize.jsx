c94da3def787d9ffe3751ab5a471a82fc2764227
shogo-okugami <okugami365@gmail.com>
2021-05-31 08:28:07 +0900

    update:ダークモードに対応 ダークモードに対応

--- resources/js/components/Settings/FontSize.jsx
+++ resources/js/components/Settings/FontSize.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { AppSettings } from '../App'
 
 const FontSize = () => {
     const [isShow, setIsShow] = useState(false)
     const sizes = ['standard', 'medium', 'large']
     const menuRef = useRef(null)
+    const isDark = useContext(AppSettings).darked.isDark
 
     const handleClick = (value) => {
         localStorage.setItem('font_size', value)
@@ -30,7 +32,7 @@ const FontSize = () => {
                 change size
             </button>
             {isShow &&
-                <ul className='p-settings__menu' ref={menuRef} tabIndex={0} onBlur={() => setIsShow(false)}>
+                <ul className={'p-settings__menu' + (isDark ? ' is-dark' : '')} ref={menuRef} tabIndex={0} onBlur={() => setIsShow(false)}>
                     {sizes.map((value, index) => <li onClick={() => handleClick(value)} key={index}>{value}</li>)}
                 </ul>
             }
