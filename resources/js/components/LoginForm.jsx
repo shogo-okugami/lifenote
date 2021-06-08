ba2d10c3b52bba6c1589ac70d8cfc5ece141b76f
shogo-okugami <okugami365@gmail.com>
2021-06-08 22:04:44 +0900

    add:ログイン画面のコンポーネント ログイン画面をコンポーネント化

---
+++ resources/js/components/LoginForm.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {route} from '../functions'
+
+const LoginForm = ({csrf}) => {
+
+    return (
+        <div className="l-wrapper__inner">
+            <form method="POST" className="c-form" action={route('login')}>
+                <input type='hidden' name='_token' value={csrf} readOnly/>
+                <div className="c-form__item">
+                    <input id="email" type="text" className="c-form__input @error('email') is-invalid @enderror" placeholder="E-mail Adress" name="email" defaultValue="" required />
+                    <span className="invalid-feedback" role="alert">
+                        <strong>{ }</strong>
+                    </span>
+                </div>
+                <div className="c-form__item u-mb0">
+                    <input id="password" type="password" className="c-form__input @error('password') is-invalid @enderror" placeholder="Password" name="password" required autoComplete="current-password" />
+                    <span className="invalid-feedback" role="alert">
+                        <strong>{ }</strong>
+                    </span>
+                </div>
+                <div className="c-form__item u-mb0">
+                    <div className="c-form__password__check">
+                        <input className="form-check-input" type="checkbox" name="remember" id="remember" />
+                        <label className="form-check-label" htmlFor="remember">
+                            Remember Me
+                        </label>
+                    </div>
+                </div>
+                <button type="submit" className="c-btn--primary c-btn--small">Login</button>
+                <a className="btn btn-link" href="{{ route('password.request') }}">Forget Your Password?</a>
+            </form>
+        </div>
+    )
+}
+export default LoginForm
