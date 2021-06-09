9312048aaf189de74a0dfe93b2aade22accc1ad0
shogo-okugami <okugami365@gmail.com>
2021-06-09 19:26:05 +0900

    add:LoginFormをAuthディレクトリへ移動 LoginFormをAuthディレクトリへ移動

--- resources/js/components/Auth/LoginForm.jsx
+++ resources/js/components/Auth/LoginForm.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import {route} from '../functions'
+import { route } from '../../functions'
 
-const LoginForm = ({csrf}) => {
-
+const LoginForm = ({ csrf }) => {
     return (
         <div className="l-wrapper__inner">
             <form method="POST" className="c-form" action={route('login')}>
-                <input type='hidden' name='_token' value={csrf} readOnly/>
+                <input type='hidden' name='_token' value={csrf} readOnly />
                 <div className="c-form__item">
                     <input id="email" type="text" className="c-form__input @error('email') is-invalid @enderror" placeholder="E-mail Adress" name="email" defaultValue="" required />
                     <span className="invalid-feedback" role="alert">
