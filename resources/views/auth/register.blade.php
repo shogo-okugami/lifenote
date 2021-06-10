f0418aecc95d0331e4d8d79bf725462efe32c935
shogo-okugami <okugami365@gmail.com>
2021-06-11 08:28:57 +0900

    update:propsとして渡すための各属性を設定 Appのpropsに渡すための属性を設定

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,39 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<header id="header" class="l-header">
-    <div class="l-header__inner">
-        <h1 class="c-heading--large">lifenote</h1>
-    </div>
-</header>
-
-<main id="main" class="l-wrapper">
-    <div class="l-wrapper__inner--small">
-        <form method="POST" class="c-form" action="{{ route('register') }}">
-            @csrf
-            <div class="c-form__item">
-                <input id="email" type="email" class="c-form__input @error('email') is-invalid @enderror" placeholder="E-mail Adress" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-            </div>
-            <div class="c-form__item">
-                <input id="password" type="password" class="c-form__input @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="current-password">
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-            </div>
-            <div class="c-form__item">
-                <input id="password-confirm" type="password" class="c-form__input" name="password_confirmation" placeholder="Password Confirm" required autocomplete="new-password">
-            </div>
-            <button type="submit" class="c-btn--primary c-btn--small">
-                {{ __('Resister') }}
-            </button>
-        </form>
-    </div>
-</main>
+<div id="app"
+ isLogin='@json(Auth::check())'
+ csrf='@json(csrf_token())'
+ content='@json('register')'
+ errors='@json($errors->toArray())'>
+</div>
 @endsection
