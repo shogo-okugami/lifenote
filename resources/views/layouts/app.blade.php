ef80da87fb8162ff67241ce4a8e28da42c31327e
shogo-okugami <okugami365@gmail.com>
2021-06-15 22:39:19 +0900

    update:titleタグに@yield('title')を指定

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,6 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Laravel') }}</title>
-
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
 
@@ -17,10 +15,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 
-
-
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <title>@yield('title')</title>
 </head>
 
 <body>
