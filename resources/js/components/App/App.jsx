1aca16931e1128ddd41a014e383da0ccd9a06a80
shogo-okugami <okugami365@gmail.com>
2021-08-11 19:30:30 +0900

    update:Wrapperをレンダリングする useAppでreturnされたmainをpropsに渡して画面を描画

--- resources/js/components/App/App.jsx
+++ resources/js/components/App/App.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import { useApp } from './useApp'
-import { getClassName, getDark, getTheme } from '../../functions'
-import Navigation from '../Navigation/Navigation'
 import Header from '../Header/Header'
+import Wrapper from '../Wrapper/Wrapper'
 
 const App = ({ userId, isLogin, csrf: token, content, errors, date, notes, note }) => {
 
-    const { AppContext, isDark, setIsDark, theme, setTheme, setFont, autoDarked, setAutoDarked, csrf, mediaScreenL, main, setPaddingBottom } = useApp(isLogin, token, content, errors, date, notes, note)
+    const { AppContext, isDark, setIsDark, theme, setTheme, setFont, autoDarked, setAutoDarked, csrf, mediaScreenL, main } = useApp(isLogin, token, content, errors, date, notes, note)
 
     return (
         <AppContext.Provider value={{ user: { userId, isLogin }, darked: { isDark, setIsDark }, theme: { theme, setTheme }, font: { setFont }, autoDarked: { autoDarked, setAutoDarked }, csrf, mediaScreenL }} >
             <Header />
-            <div id='wrapper' className={'l-wrapper' + (getClassName(mediaScreenL, 'is-row')) + (getDark(isDark, isLogin)) + (getTheme({ theme, isDark, isLogin }))}>
-                {isLogin && <Navigation setPaddingBottom={setPaddingBottom} />}
-                <main id="main" className="l-wrapper__inner">
-                    {main}
-                </main>
-            </div>
+            <Wrapper main={main} />
         </AppContext.Provider>
     )
 }
